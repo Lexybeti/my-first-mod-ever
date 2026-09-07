54ef2b94e4ed7cafcd91183bde10854d26d9ec1c
Alexya Mello <elizabethalexya@gmail.com>
2026-09-07 15:15:17 -0300

    feat: new Jade Ores, Loot Tables and pickaxe/mineable setup!

--- forge-1.21-51.0.33-mdk/src/main/java/com/lexybeti/testmod/block/ModBlocks.java
+++ forge-1.21-51.0.33-mdk/src/main/java/com/lexybeti/testmod/block/ModBlocks.java
@@ -2,9 +2,11 @@ package com.lexybeti.testmod.block;
 
 import com.lexybeti.testmod.TestMod;
 import com.lexybeti.testmod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,19 +22,19 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> JADE_BLOCK = registerBlock("jade_block",
             () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(3f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
+                    .strength(2f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
 
     public static final RegistryObject<Block> RAW_JADE_BLOCK = registerBlock("raw_jade_block",
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(2f).requiresCorrectToolForDrops().sound(SoundType.CALCITE)));
 
     public static final RegistryObject<Block> JADE_ORE_BLOCK = registerBlock("jade_ore_block",
-            () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f).requiresCorrectToolForDrops()));
+            () -> new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of()
+                    .strength(3f).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> DEEPSLATE_JADE_ORE_BLOCK = registerBlock("deepslate_jade_ore_block",
-            () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(5f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
+            () -> new DropExperienceBlock(UniformInt.of(2, 6), BlockBehaviour.Properties.of()
+                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
